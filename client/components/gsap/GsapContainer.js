6af78f1b3d20469ad1f206fe2e927fc4ce311a3f
Vishal Sharma <vish.sharma0203@gmail.com>
2017-05-24 17:03:52 +0530

    playing with tweenlite

--- client/components/gsap/GsapContainer.js
+++ client/components/gsap/GsapContainer.js
@@ -12,12 +12,20 @@ class LandingContainer extends Component {
   componentDidMount() {
     setTimeout(() => {
       this.animateBox()
-    }, 200);
+    }, 500);
   }
 
   animateBox() {
     const box = ReactDOM.findDOMNode(this.refs.box);
-    TweenLite.to(box, 0.7, {left: 0});
+    // TweenLite.to(box, 0.7, {left: 0, x: 0});
+
+    // TweenLite.from(box, 2, {x: '-=200px', autoAlpha: 0});
+
+    // TweenLite.set(box, {x: '-=200px', scale: 0.3});
+    // TweenLite.set(box, {x: '+=100px', scale: 0.6, delay: 1});
+    // TweenLite.set(box, {x: '-50%', scale: 1, delay: 2});
+
+    TweenLite.fromTo(box, 2, {x: '-=200px'}, {x: 150});
   }
 
   render() {
